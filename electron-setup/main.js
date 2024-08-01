b17ae982d9511d0b4630fc7e33f7dcaa1574b1bc
david-ebenezer <david.ebenezer.2004@gmail.com>
2024-08-01 19:21:20 +0530

    version update

--- electron-setup/main.js
+++ electron-setup/main.js
@@ -4,8 +4,8 @@ const path = require('path');
 function createWindow() {
   // Create the main window
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
@@ -18,10 +18,10 @@ function createWindow() {
 
   // Create the display window without a menu bar
   const screen1Window = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     x: 1920, // Position it on the second screen (adjust as needed)
-    frame: false, // Remove the frame and menu bar
+    frame: true, // Remove the frame and menu bar
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
@@ -32,6 +32,24 @@ function createWindow() {
 
   // Load the display HTML
   screen1Window.loadFile(path.join(__dirname, 'public/screen1.html'));
+
+  // Create another display window for screen2.html
+  // const screen2Window = new BrowserWindow({
+  //   width: 1920,
+  //   height: 1080,
+  //   x: 1920, // Adjust position as needed
+  //   y: 600, // Adjust position as needed
+  //   frame: true, // Remove the frame and menu bar
+  //   webPreferences: {
+  //     preload: path.join(__dirname, 'preload.js'),
+  //     contextIsolation: true,
+  //     enableRemoteModule: false,
+  //     nodeIntegration: false
+  //   }
+  // });
+
+  // // Load the display HTML for screen2
+  // screen2Window.loadFile(path.join(__dirname, 'public/screen2.html'));
 }
 
 app.whenReady().then(() => {
